120e196f610710bb14cb24ba93dd8d29816432a2
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-08-08 17:36:17 -0700

    adding if statement to test character length selection

--- script.js
+++ script.js
@@ -2,19 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // Character lists
-var specialCharacters = ['!','"','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
-
-// Character prompts
-var charLength = prompt('Please choose a character length between 8 and 128 characters');
-var includeSpecial = prompt('Would you like to include Special Characters?').toLowerCase();
-var includeUpper = prompt('Would you like to include Upper Case letters?').toLowerCase();
-var includeLower = prompt('Would you like to include Lower Case letters?').toLowerCase();
-
-
-
+let specialCharacters = ['!','"','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let passChar = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -22,12 +13,33 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword() {
+  // Character length prompt
+  var passLength = prompt('Please choose a character length between 8 and 128 characters');
+  // Tests that character length is within acceptable range
+  if (passLength < 8 || passLength > 128) {
+    alert('Please choose a character length between 8 and 128');
+    } 
+
+  var includeUpper = prompt('Would you like to include Upper Case letters?').toLowerCase();
+  var includeLower = prompt('Would you like to include Lower Case letters?').toLowerCase();
+  var includeSpecial = prompt('Would you like to include Special Characters?').toLowerCase();
+  
 
 }
+
+
+  
+  
+
+
+
+
+
+
+
 
 
 
